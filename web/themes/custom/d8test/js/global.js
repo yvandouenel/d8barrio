1274d64c8b0db42b4a94caceee6204aadf4d2616
yvan <y.douenel@coopernet.fr>
2019-12-04 09:47:49 +0100

    Theme barrio added.

---
+++ web/themes/custom/d8test/js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.d8test = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(jQuery, Drupal);
